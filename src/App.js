cf3c06d789878327d0f2f5f89909b36c20aa3657
Michel Pereira <michelbecauro@gmail.com>
2021-12-16 17:44:43 -0300

    feat: Define rotas em App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import './css/App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Cart from './components/Cart';
+import Product from './components/Product';
+import FormNewProduct from './components/FormNewProduct';
+import FormNewUser from './components/FormNewUser';
+import Checkout from './components/Checkout';
+ 
 function App() {
-  return (
-    <div className="App">
-     <span> Something</span>
-    </div>
-  );
+ return (
+     <Routes>
+       <Route exact path="/" element={ <Home/> } />
+       <Route path="/login" element={ <Login /> } />
+       <Route path="/cart" element={ <Cart /> } />
+       <Route path="/produtos/:id" element={ <Product/>} />
+       <Route path="/produtos/new" element={ <FormNewProduct/>} />
+       <Route path="/users/new" element={ <FormNewUser/>} />
+       <Route path="/checkout" element={ <Checkout/>} />
+     </Routes>
+ );
 }
-
+ 
 export default App;
