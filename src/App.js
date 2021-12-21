0b8d4aadc443b7a8d1d77e1e65308e235eb42901
Michel Pereira <michelbecauro@gmail.com>
2021-12-21 13:29:50 -0300

    chore: Rename 'FormNewUser' page to 'UserRegistrationForm'
     This also renames every relationship with that

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Login from './pages/Login';
 import Cart from './pages/Cart';
 import Product from './pages/Product';
 import FormNewProduct from './pages/FormNewProduct';
-import FormNewUser from './pages/FormNewUser';
+import UserRegistrationForm from './pages/UserRegistrationForm';
 import Checkout from './pages/Checkout';
 
 function App() {
@@ -16,10 +16,10 @@ function App() {
        <Route exact path="/" element={ <Home/> } />
        <Route path="/login" element={ <Login /> } />
        <Route path="/cart" element={ <Cart /> } />
-       <Route path="/product/:id" element={ <Product/>} />
-       <Route path="/product/new" element={ <FormNewProduct/>} />
-       <Route path="/user/new" element={ <FormNewUser/>} />
-       <Route path="/checkout" element={ <Checkout/>} />
+       <Route path="/product/:id" element={ <Product/> } />
+       <Route path="/product/new" element={ <FormNewProduct/> } />
+       <Route path="/user/registration" element={ <UserRegistrationForm/> } />
+       <Route path="/checkout" element={ <Checkout/> } />
     </Routes>
 
  );
