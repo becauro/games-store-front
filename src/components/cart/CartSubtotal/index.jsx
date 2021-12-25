cca0bda8d11bd68f6348e1178b94eafc50ee9157
Michel Pereira <michelbecauro@gmail.com>
2021-12-25 19:50:13 -0300

    feat: Add 'To checkout' button in Cart page

--- src/components/cart/CartSubtotal/index.jsx
+++ src/components/cart/CartSubtotal/index.jsx
@@ -1,53 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import './CartSubtotal.css';
 
 /* eslint-disable */
 
-class CartSubtotal extends Component {
+function CartSubtotal ({ cartReducer: { sumSubTotal, sumQuantity } }) {
+  const navigate = useNavigate();
 
-  constructor() {
-    super();
-
-    this.state = {
-      sumSubTotal: 0,
-      sumQuantity: 0
-    }
-
-    this.sumItemsSubtotal = this.sumItemsSubtotal.bind(this);
-
+  const toCheckoutOnclick = () => {
+    navigate('/checkout');
   }
-
-  componentDidMount() {
-    this.sumItemsSubtotal();
-  }
-
-  sumItemsSubtotal () {
-    const { 
-      cartReducer: { 
-        cartProducts,
-      },
-    } = this.props;
-
-    const sumQuantity = cartProducts.reduce((acc, item) => ( acc + parseInt(item.quantity)), 0 );
-    const sumSubTotal = cartProducts.reduce((acc, item) => (acc + parseInt(item.productSubtotal)), 0 );
-    this.setState({
-      sumSubTotal,
-      sumQuantity
-    });
-  }
-
-
-
-  render() {
-    const { cartReducer: { sumSubTotal, sumQuantity } } = this.props;
 
     return (
       <div className="cart-subtotal">
         <p>SubTotal ({sumQuantity} Item(s)): <span>R$ { sumSubTotal } </span></p> 
+        <button onClick={ () => toCheckoutOnclick()}>
+          Proceed to Checkout
+        </button>
       </div>
     );
-  }
 }
 
 const mapStateToProps = ({ cart: cartReducer }) => ({ cartReducer });
