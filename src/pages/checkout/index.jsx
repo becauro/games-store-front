10a0960e65d81652b98e6040ba3f40e9672cbd23
Michel Pereira <michelbecauro@gmail.com>
2021-12-29 03:09:12 -0300

    feat: Add warning beside Buy button (Checkout page)
    The warning use checkEmptyField() logic inside handleChangedState()

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './checkout.css';
 
-const fullnameRegexp = new RegExp('[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]{3}');
-const cpfRegexp = new RegExp('^([0-9]){3}.([0-9]){3}.([0-9]){3}-([0-9]){2}$');
-const emailRegexp = new RegExp('\\S+@\\S+\\.\\S+');
-const phoneRegexp = new RegExp('\\d{2}\\s\\d{4,5}\\-\\d{4}$');
-const cepRegexp = new RegExp('[0-9]{5}-[0-9]{3}$');
-const addressRegxp = new RegExp('[a-zA-Z0-9 ,.]{10}');
+// const fullnameRegexp = new RegExp('[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]{3}');
+// const cpfRegexp = new RegExp('^([0-9]){3}.([0-9]){3}.([0-9]){3}-([0-9]){2}$');
+// const emailRegexp = new RegExp('\\S+@\\S+\\.\\S+');
+// const phoneRegexp = new RegExp('\\d{2}\\s\\d{4,5}\\-\\d{4}$');
+// const cepRegexp = new RegExp('[0-9]{5}-[0-9]{3}$');
+// const addressRegxp = new RegExp('[a-zA-Z0-9 ,.]{10}');
+
 
 export default class Checkout extends React.Component {
   constructor() {
@@ -19,43 +20,88 @@ export default class Checkout extends React.Component {
       phone: '',
       cep: '',
       address: '',
-      method: ' ',
+      method: '',
       fulled: false,
-
+      valid: false,
+      all_ok: false,
+      errorMsg: 'There is required field empty'
     };
     this.handleCheck = this.handleCheck.bind(this);
-    this.handle = this.handle.bind(this);
+    this.onChangeFieldHandler = this.onChangeFieldHandler.bind(this);
   }
 
-  componentDidMount() {
-    this.getDataFromcartReducer();
-  }
+  // componentDidMount() {
+  //   this.getDataFromcartReducer();
 
-  handleCheck() {
-    const { fullname, email, cpf, phone, cep, address, method } = this.state;
-    if (fullnameRegexp.test(fullname) && cpfRegexp.test(cpf) && emailRegexp.test(email)
-    && phoneRegexp.test(phone) && cepRegexp.test(cep) && addressRegxp.test(address)
-    && method !== ' ') {
-      this.setState({ fulled: true });
+  // }
+
+  handleCheck = () => {
+    const { fulled, valid } = this.state;
+
+    if (fulled || valid) {
+      this.setState({
+        all_ok: true
+      })
     }
   }
 
-  getDataFromcartReducer = () => {
-    const subtotal = 'get value from cart reducer'
+  // checkValidField() {
+  //   const { fullname, email, cpf, phone, cep, address, method } = this.state;
+  //   if (fullnameRegexp.test(fullname) && cpfRegexp.test(cpf) && emailRegexp.test(email)
+  //   && phoneRegexp.test(phone) && cepRegexp.test(cep) && addressRegxp.test(address)
+  //   && method !== ' ') {
+  //     this.setState({ valid: true });
+  //   }
+  // }
 
-    this.setState({
-      subtotal,
-    });
+  checkEmptyField = () => {
+
+    const { fullname, email, cpf, phone, cep, address, method } = this.state;
+
+    if (fullname === '' || email === '' || cpf === ''
+    || phone === '' || cep === '' || address === '' || method === '') {
+
+      // console.log('Entrou no IF');
+      
+      this.setState({
+        errorMsg: 'There is required field empty',
+        fulled: false
+      })
+
+    } else {
+      // console.log('Entrou no ELSE');
+      
+      this.setState({
+        errorMsg: '',
+        fulled: true
+      })
+    }
   }
 
-  handle(target) {
-    this.setState({ [target.name]: target.value });
+  // getDataFromcartReducer = () => {
+  //   const subtotal = 'get value from cart reducer'
+
+  //   this.setState({
+  //     subtotal,
+  //   });
+  // }
+
+
+  handleChangedState = () => {
+    this.checkEmptyField();
+  }
+
+  onChangeFieldHandler(target) {
+    this.setState({ 
+      [target.name]: target.value,
+    }, this.handleChangedState);
   }
 
   render() {
-    const { fulled, subtotal, fullname, email, cpf, phone, cep, address } = this.state;
+    const { all_ok, subtotal, fullname, email, cpf, phone, cep, address, errorMsg } = this.state;
 
-    if (fulled) {
+
+    if (all_ok) {
       return < h1> Thank you! </h1>;
     }
 
@@ -69,7 +115,7 @@ export default class Checkout extends React.Component {
             </h2>
           </fieldset>
           <fieldset>
-            <legend><h2>Customer data</h2></legend>
+            <legend><h2>Customer data *</h2></legend>
             <input
               name="fullname"
               value={ fullname }
@@ -78,7 +124,7 @@ export default class Checkout extends React.Component {
               pattern="[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]{3,}"
               title="At least 3 characters"
               placeholder="fullname"
-              onChange={ ({ target }) => this.handle(target) }
+              onChange={ ({ target }) => this.onChangeFieldHandler(target) }
             />
             <input
               name="email"
@@ -88,7 +134,7 @@ export default class Checkout extends React.Component {
               title="Format: nick@something.something"
               placeholder="E-mail"
               maxLength="50"
-              onChange={ ({ target }) => this.handle(target) }
+              onChange={ ({ target }) => this.onChangeFieldHandler(target) }
             />
             <input
               name="cpf"
@@ -98,7 +144,7 @@ export default class Checkout extends React.Component {
               title="Format: 000.000.000-00"
               placeholder="CPF"
               maxLength="14"
-              onChange={ ({ target }) => this.handle(target) }
+              onChange={ ({ target }) => this.onChangeFieldHandler(target) }
             />
             <input
               name="phone"
@@ -108,7 +154,7 @@ export default class Checkout extends React.Component {
               title="00 0000-0000 or 00 00000-0000"
               placeholder="Phone Number"
               maxLength="14"
-              onChange={ ({ target }) => this.handle(target) }
+              onChange={ ({ target }) => this.onChangeFieldHandler(target) }
             />
             <input
               name="cep"
@@ -119,7 +165,7 @@ export default class Checkout extends React.Component {
               minLength="9"
               maxLength="9"
               placeholder="CEP"
-              onChange={ ({ target }) => this.handle(target) }
+              onChange={ ({ target }) => this.onChangeFieldHandler(target) }
             />
             <input
               name="address"
@@ -129,26 +175,27 @@ export default class Checkout extends React.Component {
               title="At least 10 characters"
               maxLength="50"
               placeholder="Address"
-              onChange={ ({ target }) => this.handle(target) }
+              onChange={ ({ target }) => this.onChangeFieldHandler(target) }
             />
           </fieldset>
           <fieldset>
-            <legend><h2>Payment Method</h2></legend>
+            <legend><h2>Payment Method *</h2></legend>
             <input
               name="method"
               type="radio"
               value="credit-card"
-              onChange={ ({ target }) => this.handle(target) }
+              onChange={ ({ target }) => this.onChangeFieldHandler(target) }
               id="credit-card-payment"
             />
               <label htmlFor="credit-card-payment">Credit Card</label>
-            <input id="debit-card-payment" name="method" type="radio" value="debit-card" onChange={ ({ target }) => this.handle(target) } />
+            <input id="debit-card-payment" name="method" type="radio" value="debit-card" onChange={ ({ target }) => this.onChangeFieldHandler(target) } />
               <label htmlFor="debit-card-payment" >Debit Card</label>
-            <input id="btc-payment" name="method" type="radio" value="btc" onChange={ ({ target }) => this.handle(target) } />
+            <input id="btc-payment" name="method" type="radio" value="btc" onChange={ ({ target }) => this.onChangeFieldHandler(target) } />
               <label htmlFor="btc-payment" >BTC</label>
             
           </fieldset>
           <button id="buy-button" type="button" onClick={ this.handleCheck }>BUY</button>
+          <span>{ errorMsg }</span>
         </form>
       </div>
     );
