5147439754b59d0358b42814b8903ca4830bf1dc
Michel Pereira <michelbecauro@gmail.com>
2021-12-29 11:44:32 -0300

    feat: 'CartSubtotal' handle with empty Cart in ...
     ... Cart page

--- src/components/cart/CartSubtotal/index.jsx
+++ src/components/cart/CartSubtotal/index.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import './CartSubtotal.css';
 
 /* eslint-disable */
 
-function CartSubtotal ({ cartReducer: { sumSubTotal, sumQuantity } }) {
+function CartSubtotal ({ cartReducer: { cartProducts, sumSubTotal, sumQuantity } }) {
   const navigate = useNavigate();
+  const [cartEmpty, setEmpty] = useState(true)
+  const [cartEmptyMsg, setEmptyMsg] = useState('')
+
+  useEffect(() => {
+    if (cartProducts.length === 0) {
+      setEmpty(true);
+    } else { setEmpty(false) };
+  });
 
   const toCheckoutOnclick = () => {
-    navigate('/checkout');
+    if (cartEmpty === false) {
+      navigate('/checkout');
+    } else {
+      setEmptyMsg('Cannot checkout from empty cart')
+    }
   }
 
     return (
       <div className="cart-subtotal">
         <p>SubTotal ({sumQuantity} Item(s)): <span>R$ { sumSubTotal } </span></p> 
-        <button onClick={ () => toCheckoutOnclick()}>
+        <button type="button" onClick={ () => toCheckoutOnclick()}>
           Proceed to Checkout
         </button>
+        <span>{cartEmptyMsg}</span>
       </div>
     );
 }
