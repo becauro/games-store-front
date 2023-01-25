b357e6b6579f85c4123c077ea0949acd70edf595
Michel Pereira <michelbecauro@gmail.com>
2023-01-25 11:52:44 -0300

    FEAT: Implements Front without Back option

--- src/api/backend_api.js
+++ src/api/backend_api.js
@@ -1,15 +1,32 @@
-const HOST = process.env.REACT_APP_API_HOST;
-const PORT = process.env.REACT_APP_API_PORT;
+import axios from "axios";
+
+const PORT = process.env.REACT_APP_API_PORT || 3001;
+const HOST = process.env.REACT_APP_API_HOST || "localhost";
+const BACK = process.env.REACT_APP_WITH_BACK || "false" ;
 
 export async function fetchAllProducts() {
  
-  const ENDPOINT = `http://${HOST}:${PORT}/products`;
-
   try {
+  
+    let response;
 
-    const response = await fetch(ENDPOINT);
+    if (BACK == "false") {
+    
+        console.log('It is running WITHOUT backend');
+       
+        response = await axios.get('local_data.json');
+       
+        return response.data;
+        
+   } else { 
+    
+        console.log('It is running WITH backend');
 
-    return response.json();
+        const ENDPOINT = `http://${HOST}:${PORT}/products`;
+        response = await fetch(ENDPOINT, {mode: "cors"});
+
+        return response.json();
+    }
   
   } catch (error) {
 
